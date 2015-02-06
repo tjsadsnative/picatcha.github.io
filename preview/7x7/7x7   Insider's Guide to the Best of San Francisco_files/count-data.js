611bd8fdf65143989a95012d9e8b53c81771a617
Mitch <mitchkarns@berkeley.edu>
2015-02-06 15:10:04 -0800

    added 7x7 ad

---
+++ preview/7x7/7x7   Insider's Guide to the Best of San Francisco_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"node\/153515","comments":0},{"id":"node\/153351","comments":0},{"id":"node\/153403","comments":0},{"id":"node\/153514","comments":0},{"id":"node\/153536","comments":0},{"id":"node\/153526","comments":1},{"id":"node\/153528","comments":0},{"id":"node\/153529","comments":3},{"id":"node\/153538","comments":0},{"id":"node\/153525","comments":0}]});
+}
