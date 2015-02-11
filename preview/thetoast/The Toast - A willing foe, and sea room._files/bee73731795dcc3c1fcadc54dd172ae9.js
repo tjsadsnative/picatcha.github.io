26d07a0d3ea257d14dbdd1718f5ba950ed5545b7
Mitch <mitchkarns@berkeley.edu>
2015-02-10 16:19:33 -0800

    added thetoast page

---
+++ preview/thetoast/The Toast - A willing foe, and sea room._files/bee73731795dcc3c1fcadc54dd172ae9.js
@@ -0,0 +1,3 @@
+var kid='bee73731795dcc3c1fcadc54dd172ae9';document.write('<div id="kmn_tag_'+kid+'" class="kmn-div">');
+kmn_sa.spark(kid,{codes:'o1,e1,p1,t1,h',srv:'a1'});kmn_data[kid]={chain:'N2',codes:['o1','e1','p1','t1','h'],full_index:1,order:'1o',w:300,h:250};kmn_sa.write_js('//adserver.adtechus.com/addyn/3.0/9423.1/3402595/0/170/ADTECH;loc=100;target=_blank;misc=[TIMESTAMP];rdclick=[CLICKMACRO]');
+document.write('</div>');
