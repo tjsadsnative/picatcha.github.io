180f3676138a892786392319919472b21334fdd5
Mitch <mitchkarns@berkeley.edu>
2015-02-25 15:52:18 -0800

    testing iframe things

--- preview/test/Home - Bild.de_files/livescores-widget.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	if (jQuery('#wfb-games-today .module .module-livecalender .wfb-competition-live').length == 0)
-	{
-		jQuery('#wfb-games-today').hide();
-	}	
-});
--- preview/test/Home - Bild.de_files/mpt.js
+++
@@ -1,2 +0,0 @@
-rem_callback_82578({"data":{"tracking_id":"1494040480154894858"},"meta":{"type":"c1_result"}}
-)
