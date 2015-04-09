a116580477c16b22c59bd5a388c2eee2e5602941
Mitch <mitchkarns@berkeley.edu>
2015-04-09 16:32:12 -0700

    retrying the atlantic

---
+++ preview/theatlantic/the_atlantic_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"<span class=\"disqus-no-comments\">&nbsp;<\/span> ","multiple":"<span class=\"disqus-comment-count\">{num}<\/span>","one":"<span class=\"disqus-comment-count\">1<\/span> "}},"counts":[{"id":"mt390051","comments":7},{"id":"mt390072","comments":59},{"id":"mt390108","comments":714},{"id":"mt390087","comments":55},{"id":"mt390084","comments":71},{"id":"mt390036","comments":16},{"id":"mt390081","comments":28},{"id":"mt390111","comments":7}]});
+}
