83a1fa778cc51705cb3e1885c18bdf44898fd0c0
Mitch <mitchkarns@berkeley.edu>
2015-04-09 16:37:15 -0700

    removed the atlantic

--- preview/the_atlantic/In Season Five on FX, the Glorious Return of Sad-Sack 'Louie' — The Atlantic_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"<span class=\"disqus-no-comments\">&nbsp;<\/span> ","multiple":"<span class=\"disqus-comment-count\">{num}<\/span>","one":"<span class=\"disqus-comment-count\">1<\/span> "}},"counts":[{"id":"mt390051","comments":6},{"id":"http:\/\/www.theatlantic.com\/entertainment\/archive\/2015\/04\/the-glorious-return-of-sad-sack-louie\/390051\/","comments":6}]});
-}
