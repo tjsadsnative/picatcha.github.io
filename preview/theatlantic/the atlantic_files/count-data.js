91a3fda62c20c4ed91698775e3ede8c1c2c4911c
Mitch <mitchkarns@berkeley.edu>
2015-04-09 16:01:53 -0700

    added atlantic home page

---
+++ preview/theatlantic/the atlantic_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"<span class=\"disqus-no-comments\">&nbsp;<\/span> ","multiple":"<span class=\"disqus-comment-count\">{num}<\/span>","one":"<span class=\"disqus-comment-count\">1<\/span> "}},"counts":[{"id":"mt390051","comments":6},{"id":"mt390072","comments":54},{"id":"mt390108","comments":709},{"id":"mt390087","comments":44},{"id":"mt390084","comments":70},{"id":"mt390036","comments":15},{"id":"mt390081","comments":26},{"id":"mt390111","comments":7}]});
+}
